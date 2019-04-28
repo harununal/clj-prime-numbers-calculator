bd209b32b23cf0439c141585748e0c22d8bf590b
Harun Ünal <36730353+harununal@users.noreply.github.com>
2019-04-29 02:14:15 +0300

    Update prime-num-list-maker.clj

--- prime-num-list-maker.clj
+++ prime-num-list-maker.clj
@@ -1,31 +1,19 @@
 
 (defn fold-?
-"
- This function takes a *prime-numbers list* and creates an anonymous function.
+"This function takes a *prime-numbers list* and creates an anonymous function.
  Unnamed function checks if a number is fold to any numbers in prime-numbers list.
- 
- (map (fold-? 20) [2 3 5 7])
-;=> (true false true false)
- 
- "
+ (map (fold-? 20) [2 3 5 7]) ;=> (true false true false) "
 [pn-?]
 #(integer? (/ pn-? %)))
 
 
 
-(defn pnl-number-list
-"
- This function take a number and create a prime numbers list 
+(defn pnl<=x
+"This function take a number and create a prime numbers list 
  that smaller than this number or equal to this
- 
- (pnl-number-list 20)
- ;=> [2 3 5 7 11 13 17 19]
- 
- (pnl-number-list 7)
- ;=> [2 3 5 7]
- 
- "
-[max-val]
+ (pnl-number-list 20) ;=> [2 3 5 7 11 13 17 19]
+ (pnl-number-list 7) ;=> [2 3 5 7] "
+[max-val
 (if (>= max-val 2)
     (loop [pn-list [2]
            pn-? 3]
@@ -40,14 +28,14 @@
 ;;;;;;;;;;;;;;;;;; fold-? and pnl-number-list is inseperable ;;;;;;;;;;;;;;;;;;;;
 
 (defn prime-?
-"
- This function is check if a number is a prime-number.
- 
- (prime-? 7919) ;=> true
- 
- "
-[x]
-(= (apply max (pnl-number x)) x))
+"This function is check if a number is a prime-number.
+ (prime-? 7919) ;=> true "
+[a-num]
+(= (apply max (pnl<=x a-num)) a-num))
 
-
-
+(defn nth-prime
+""
+[a-num]
+(if prime-?
+(str a-num " is " (count (pnl<=x a-num)) ". prime-number.")
+("This is not a prime number.")))
