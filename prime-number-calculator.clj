6d0fa8c427bc34b62d2d3a3f1fa5d18754ae8ed0
Harun Ünal <36730353+harununal@users.noreply.github.com>
2019-04-29 04:28:19 +0300

    Update prime-number-calculator.clj

--- prime-number-calculator.clj
+++ prime-number-calculator.clj
@@ -6,7 +6,7 @@
 [pn-?]
 #(integer? (/ pn-? %)))
 
-
+; (map (fold-? 20)[2 3 5 7])
 
 (defn pnl-maker
 "This function take a number and create a prime numbers list 
@@ -24,24 +24,32 @@
                         pn-list
                         (conj pn-list pn-?))
                     (inc pn-?))))
-    (str "Please insert an integer bigger than 2 or equal")))
+    (str "pnl-maker :" "Please insert an integer bigger than 2 or equal")))
 
 ;;;;;;;;;;;;;;;;;; fold-? and pnl-number-list is inseperable ;;;;;;;;;;;;;;;;;;;;
+
+;(pnl-maker 7)
 
 (defn prime-?
 "This function is check if a number is a prime-number.
  (prime-? 7919) ;=> true "
 [a-num]
-(= (apply max (pnl-maker a-num)) a-num))
-
+(if (and (integer? a-num)(>= a-num 2))
+    (= (apply max (pnl-maker a-num)) a-num)
+    (prn "prime-? :""Please insert an integer bigger than 2 or equal")))
+(prime-? 7)
 ;;;;;;;;;;;;;
 
 (defn nth-prime
 "This function given that what a number's ranking in prime numbers list.
-(nth-prime 17) ;=>
+(nth-prime 17) ;=>;;
 17 is 7. prime-number
 [2 3 5 7 11 13 17] "
 [a-num]
-(if prime-?
-(str a-num " is " (count (pnl-maker a-num)) ". prime-number.")
-("This is not a prime number.")))
+(if (and (integer? a-num)(>= a-num 2)(prime-? a-num))
+(do (prn (str a-num " is " (count (pnl-maker a-num)) ". prime-number."))
+(prn (pnl-maker a-num)))
+(prn "This is not a prime number.")))
+
+(nth-prime 7)
+(nth-prime 6)
