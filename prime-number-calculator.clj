9648af69fbacf0b9251ef3781ca3023948b50e01
Harun Ünal <36730353+harununal@users.noreply.github.com>
2019-04-30 17:01:51 +0300

    Update prime-number-calculator.clj

--- prime-number-calculator.clj
+++ prime-number-calculator.clj
@@ -1,90 +1,45 @@
 
-(defn fold-?
-"This function takes a *prime-numbers list* and creates an anonymous function.
- Unnamed function checks if a number is fold to any numbers in prime-numbers list.
- (map (fold-? 20) [2 3 5 7]) ;=> (true false true false) "
+(defn divisor-test
+  "Gets an integer and returns an anonymous function.
+  The function is checks if numbers in a list are aliquot/divisor for this integer.
+  (map (fold-test 20) [2 3 5 7]) ;=> (true false true false)  "
 [pn-?]
-#(integer? (/ pn-? %)))
+ #(integer? (/ pn-? %)))
 
 ;;;
 
-(defn pnl-maker
-"This function take a number and create a prime numbers list 
- that smaller than this number or equal to this
- (pnl-maker 20) ;=> [2 3 5 7 11 13 17 19]
- (pnl-maker 7) ;=> [2 3 5 7] "
+(defn prime-numbers-to
+  "Gets an integer and returns a vector : [prime-numbers < or = n ]
+  (prime-numbers-to 20) ;=> [2 3 5 7 11 13 17 19] "
 [max-val]
-(if (and (>= max-val 2) (integer? max-val))
-    (loop [pn-list [2]
+(loop [pn-list [2]
            pn-? 3]
           (if (< max-val pn-?)
               pn-list
               (recur 
-                    (if (some true? (map (fold-? pn-?) pn-list))
+                    (if (some true? (map (divisor-test pn-?) pn-list))
                         pn-list
                         (conj pn-list pn-?))
-                    (inc pn-?))))
-    (str "pnl-maker :" "Please insert an integer bigger than 2 or equal")))
+                    (inc pn-?)))))
 
 ;;;
 
 (defn prime-?
-"This function is check if a number is a prime-number.
- (prime-? 7919) ;=> true "
-[a-num]
-(if (and (integer? a-num)(>= a-num 2))
-    (= (apply max (pnl-maker a-num)) a-num)
-    (prn "prime-? :""Please insert an integer bigger than 2 or equal")))
-(prime-? 7)
+  "Checks an integer is it  prime-number or not
+  (prime-? 9973) ;=> true "
+  [an-int]
+  (= (apply max (prime-numbers-to an-int)) an-int))
 
 ;;;
 
-(defn nth-prime
-"This function given that what a number's ranking in prime numbers list.
-(nth-prime 17) ;=>;;
-17 is 7. prime-number
-[2 3 5 7 11 13 17] "
-[a-num]
-(if (and (integer? a-num)(>= a-num 2)(prime-? a-num))
-(do (prn (str a-num " is " (count (pnl-maker a-num)) ". prime-number."))
-(prn (pnl-maker a-num)))
-(prn "This is not a prime number.")))
+(map (fold-test 60)
+     [2 3 4 5 7 11 15 17 20 23 29 30 37 41 43 47])
+;=> (true true true true false false true false true false false true false false false false)
+
+(prime-numbers-to 100)
+;=>[2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97]
+
+(prime-? 9973)
+;=> true
 
 ;;;
-
-(defn div-list
- [bool-list pri-list]
- (filter (:true (map bool-list pri-list))))
-
-
-;;;
-
-(defn prn-prime-test)
-[val]
-(if (prime-? val) 
-(prn (str val " is " (count (pnl-maker val)) ". prime-number. Prime-numbers they are smaller than " val " : ")
-       (prn (pnl-maker val)))
-)
-
-
-
-(defn pnl-calc
- [val]
-(if (and (integer? val)(>val 1))
-(prn-prime-list val)
-(prn "Please insert an integer bigger than 2 or equal"))
-
-
-
- (if (and (integer? val)(> val 1)(prime-? val))
-   (if (fold-? val)
-   (prn (str val " is " (count (pnl-maker val)) ". prime-number. Prime-numbers they are smaller than " val " : ")
-       (prn (pnl-maker val)))
-       
-  (if (integer? val)
-   (prn (str val " is not a prime-number. " val " can be divided this prime-numbers : ")
-       (div-list (fold-? val) (pnl-maker val))))
-
-          
- (pnl-calc 17)
- 
