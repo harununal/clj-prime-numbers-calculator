23f69a323894a92f94fe691565022468bfe715ca
Harun Ünal <36730353+harununal@users.noreply.github.com>
2019-06-20 12:56:33 +0300

    Update prime-number-calculator.clj

--- prime-number-calculator.clj
+++ prime-number-calculator.clj
@@ -1,11 +1,13 @@
 
 (defn divisor-test
+"Makes a list of prime numbers they are smaller than an integer or equal"
 [pn-?]
 #(integer? (/ pn-? %)))
 
 ;;;
 
 (defn prime-numbers-to
+"Checks an integer if is it a prime-number"
 [max-val]
 (loop [pn-list [2]
        pn-? 3]
@@ -19,6 +21,7 @@
 ;;;
 
 (defn prime-?
+"Checks numbers in a list if are they aliquot/divisor for an integer "
   [an-int]
   (= (apply max (prime-numbers-to an-int)) an-int))
 
